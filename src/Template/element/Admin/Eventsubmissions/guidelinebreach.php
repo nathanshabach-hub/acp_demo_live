df49309ffffdb65dfcd2364ca4acc2f979e3a780
Nathan <nathan@example.com>
2026-05-08 15:41:11 +1000

    Improve guideline breach table layout: merge columns, add status badges, separate reason/action columns

--- src/Template/element/Admin/Eventsubmissions/guidelinebreach.php
+++ src/Template/element/Admin/Eventsubmissions/guidelinebreach.php
@@ -4,6 +4,11 @@
   #facebox.breach-facebox .content { width: 460px; padding: 0; }
   .breach-popup-header { background: #f7f7f7; border-bottom: 1px solid #ddd; padding: 14px 20px; font-size: 15px; font-weight: 600; color: #333; border-radius: 4px 4px 0 0; }
   .breach-popup-body { background: #fff; padding: 20px 25px; font-size: 14px; color: #444; line-height: 1.6; border-radius: 0 0 4px 4px; }
+  #guideline_breach td, #guideline_breach th { vertical-align: middle !important; }
+  .event-num { font-weight: 600; font-size: 13px; }
+  .event-name-sub { font-size: 11px; color: #777; margin-top: 2px; }
+  .badge-pending { background: #f0ad4e; color: #fff; padding: 3px 8px; border-radius: 3px; font-size: 11px; font-weight: 600; white-space: nowrap; }
+  .badge-approved { background: #5cb85c; color: #fff; padding: 3px 8px; border-radius: 3px; font-size: 11px; font-weight: 600; white-space: nowrap; }
 </style>
 <script type="text/javascript">
     $(document).ready(function ($) {
@@ -55,13 +60,12 @@ $this->Judgeevaluations = TableRegistry::getTableLocator()->get('Judgeevaluation
                             <th class="sorting_paging">#ID</th>
                             <th class="sorting_paging">School</th>
 							<th class="sorting_paging">Convention</th>
-                            <th class="sorting_paging">Season Year</th>
-                            <th class="sorting_paging">Event Number</th>
-                            <th class="sorting_paging">Event Name</th>
-                            <th class="sorting_paging">Group Event?</th>
+                            <th class="sorting_paging">Event</th>
                             <th class="sorting_paging">Group</th>
                             <th class="sorting_paging">Student</th>
                             <th class="sorting_paging">Judge</th>
+						<th class="sorting_paging">Status</th>
+						<th class="sorting_paging" style="text-align:center;">Reason</th>
 							<th class="action_dvv"><i class=" fa fa-gavel"></i> Action</th>
                         </tr>
                     </thead>
@@ -79,67 +83,50 @@ $this->Judgeevaluations = TableRegistry::getTableLocator()->get('Judgeevaluation
                                 <td data-title="#ID"><?php echo $datarecord->id;?></td>
 								<td data-title="School"><?php echo $datarecord->Uploadeduser['first_name'].' '.$datarecord->Uploadeduser['last_name'];?></td>
 								<td data-title="Convention"><?php echo $datarecord->Conventions['name'];?></td>
-                                <td data-title="Season Year"><?php echo $datarecord->season_year;?></td>
-								<td data-title="Event Number"><?php echo $datarecord->event_id_number;?></td>
-								<td data-title="Event Name"><?php echo $datarecord->Events['event_name'];?></td>
-								<td data-title="Group Event?"><?php echo ($datarecord->Events['group_event_yes_no'] == 1) ? "Yes" : "No"; ?></td>
-								<td data-title="Submitted For Group">
-								<?php
-								if(!empty($datarecord->group_name))
-								{
-									echo "Group ".$datarecord->group_name;
-								}
-								else
-								{
-									echo '-';
-								}
-								?>
-								</td>
-								<td data-title="Submitted For Student">
-								<?php
-								if($datarecord->student_id > 0)
-								{
-									echo $datarecord->Students['first_name'].' '.$datarecord->Students['middle_name'].' '.$datarecord->Students['last_name'];
-								}
-								else
-								{
-									echo '-';
-								}
-								?>
-								</td>
-								
-								<td data-title="Judge">
-								<?php
-								echo $datarecord->Judge['first_name'].' '.$datarecord->Judge['last_name'];
-								?>
-								</td> 
-								
+							<td data-title="Event">
+								<div class="event-num"><?php echo $datarecord->event_id_number;?></div>
+								<div class="event-name-sub"><?php echo $datarecord->Events['event_name'];?></div>
+							</td>
+							<td data-title="Group">
+							<?php
+							if(!empty($datarecord->group_name)) {
+								echo 'Group '.$datarecord->group_name;
+							} else {
+								echo '<span style="color:#aaa;">Individual</span>';
+							}
+							?>
+							</td>
+							<td data-title="Student">
+							<?php
+							if($datarecord->student_id > 0) {
+								echo $datarecord->Students['first_name'].' '.$datarecord->Students['middle_name'].' '.$datarecord->Students['last_name'];
+							} else {
+								echo '<span style="color:#aaa;">-</span>';
+							}
+							?>
+							</td>
+							<td data-title="Judge"><?php echo $datarecord->Judge['first_name'].' '.$datarecord->Judge['last_name']; ?></td>
+							<td data-title="Status">
+							<?php if($datarecord->guideline_breach == 2): ?>
+								<span class="badge-approved">Approved</span>
+							<?php else: ?>
+								<span class="badge-pending">Pending</span>
+							<?php endif; ?>
+							</td>
+							<td data-title="Reason" style="text-align:center;">
+								<?php echo '<a rel="facebox" href="#breach-reason-'.$datarecord->id.'" title="View Breach Reason" class="btn btn-info btn-xs"><i class="fa fa-eye"></i></a>'; ?>
+							</td>
                                 <td data-title="Action">
-									
-									<?php
-                                    echo '<a rel="facebox" href="#breach-reason-'.$datarecord->id.'" title="View Breach Reason" class="btn btn-info btn-xs"><i class="fa fa-eye"></i></a>';
-									
-									if($datarecord->guideline_breach == 1)
-									{
-										echo $this->Html->link('<i class="fa fa-check"></i>', ['controller' => 'eventsubmissions', 'action' => 'approveguidelinebreach', $datarecord->slug], [ 'escape' => false, 'title' => 'Approve', 'class' => 'btn btn-primary btn-xs', 'confirm' => 'Are you sure you want to approve this guideline breach?']);
-											
-										echo $this->Html->link('<i class="fa fa-times"></i>', ['controller' => 'eventsubmissions', 'action' => 'rejectguidelinebreach', $datarecord->slug], [ 'escape' => false, 'title' => 'Reject', 'class' => 'btn btn-warning btn-xs', 'confirm' => 'Are you sure you want to reject this guideline breach?']);
-									}
-									
-									if($datarecord->guideline_breach == 2)
-									{
-										echo 'Approved';
-										echo $this->Html->link('<i class="fa fa-times"></i>', ['controller' => 'eventsubmissions', 'action' => 'deleteguidelinebreach', $datarecord->slug], [ 'escape' => false, 'title' => 'Delete', 'class' => 'btn btn-danger btn-xs', 'confirm' => 'Are you sure you want to delete this guideline breach?']);
-									}
-									
-									
-									
-									?>
-									
-                                </td>
-								
-                                
-                            </tr>
+								<?php
+								if($datarecord->guideline_breach == 1) {
+									echo $this->Html->link('<i class="fa fa-check"></i>', ['controller' => 'eventsubmissions', 'action' => 'approveguidelinebreach', $datarecord->slug], ['escape' => false, 'title' => 'Approve', 'class' => 'btn btn-success btn-xs', 'confirm' => 'Are you sure you want to approve this guideline breach?']);
+									echo $this->Html->link('<i class="fa fa-times"></i>', ['controller' => 'eventsubmissions', 'action' => 'rejectguidelinebreach', $datarecord->slug], ['escape' => false, 'title' => 'Reject', 'class' => 'btn btn-warning btn-xs', 'confirm' => 'Are you sure you want to reject this guideline breach?']);
+								} elseif($datarecord->guideline_breach == 2) {
+									echo $this->Html->link('<i class="fa fa-trash"></i>', ['controller' => 'eventsubmissions', 'action' => 'deleteguidelinebreach', $datarecord->slug], ['escape' => false, 'title' => 'Delete', 'class' => 'btn btn-danger btn-xs', 'confirm' => 'Are you sure you want to delete this guideline breach?']);
+								} else {
+									echo '<span style="color:#aaa;">-</span>';
+								}
+								?>
                         <?php } ?>
                     </tbody>
                 </table>
